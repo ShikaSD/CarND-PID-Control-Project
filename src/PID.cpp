4af8079ccd61a6ac1fba3e8228c202126fe59d0f
ashikov <andrei.shikov@zalando.fi>
2017-08-22 14:53:14 +0300

    Implement PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <numeric>
 #include "PID.h"
 
 using namespace std;
@@ -11,11 +13,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    this->last_error = 0;
+    this->diff_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+    diff_error = cte - last_error;
+    last_error = cte;
+    sum_errors += cte;
 }
 
 double PID::TotalError() {
+    return -Kp * last_error - Kd * diff_error - Ki * sum_errors;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,14 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid;
+  PID steering;
+  PID acceleration;
   // TODO: Initialize the pid variable.
+  steering.Init(0.10, 0.001, 4);
+  acceleration.Init(0.1, 0.0001, 2);
+  double target_speed = 60;
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&steering, &acceleration, &target_speed](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -49,7 +53,7 @@ int main()
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+//          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
@@ -57,15 +61,20 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+          steering.UpdateError(cte);
+          steer_value = std::max(std::min(steering.TotalError(), 1.0), -1.0);
+
+          acceleration.UpdateError(speed - target_speed);
+          double throttle = std::max(std::min(acceleration.TotalError(), 1.0), -1.0);
           
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          //std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
